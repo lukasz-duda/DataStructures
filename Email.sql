5f36424e4ea8953ea928eb9aa47cb1a1a624fead
Łukasz Duda <lduda@pivotal.pl>
2020-02-24 10:36:14 +0100

    Basic structure

--- Email.sql
+++ Email.sql
@@ -11,3 +11,34 @@ GO
 
 CREATE DATABASE Email
 GO
+
+USE Email
+GO
+
+CREATE TABLE Person
+(
+    Id smallint IDENTITY PRIMARY KEY,
+    FirstName nvarchar(30) NOT NULL,
+    LastName nvarchar(50) NOT NULL,
+    EmailAddress nvarchar(100) NOT NULL
+)
+
+CREATE TABLE Email
+(
+    Id int IDENTITY PRIMARY KEY,
+    SenderId smallint FOREIGN KEY REFERENCES Person(Id) NOT NULL,
+    Topic nvarchar(50) NOT NULL,
+    Body nvarchar(1000) NOT NULL
+)
+
+CREATE TABLE CC
+(
+    EmailId int FOREIGN KEY REFERENCES Email(Id),
+    PersonId smallint FOREIGN KEY REFERENCES Person(Id)
+)
+
+CREATE TABLE BCC
+(
+    EmailId int FOREIGN KEY REFERENCES Email(Id),
+    PersonId smallint FOREIGN KEY REFERENCES Person(Id)
+)
